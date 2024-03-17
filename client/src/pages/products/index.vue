b35ea9991ae1a11f18a7a8dd761efb6c4f1cd22c
lemusm3 <lemusm3@newplatz.edu>
2024-03-17 02:23:46 -0400

    Add user list and add user components to products page

---
+++ client/src/pages/products/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+    import UserList from "@/components/UserList.vue";
+    import AddUser from '@/components/AddUser.vue';
+</script>
+<template>
+    <!-- Navigation breadcrumbs -->
+    <nav class="breadcrumb" aria-label="breadcrumbs">
+        <ul>
+            <li><a href="#">My Exercise</a></li>
+            <li><a href="#">Admin</a></li>
+            <li class="is-active"><a href="#" aria-current="page">Users</a></li>
+        </ul>
+    </nav>
+    <!-- Add a new user -->
+    <AddUser></AddUser>
+
+    <!-- Display user list -->
+    <UserList></UserList>
+</template>
+<style scoped>
+
+</style>
