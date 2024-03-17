34c89cbd481c1f357ea0ad8802c97a2e50a1f1e8
lemusm3 <lemusm3@newplatz.edu>
2024-03-17 02:22:51 -0400

    Add FriendsActivity page with workout list and add workout component

---
+++ client/src/pages/FriendsActivity.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+    import WorkoutList from '@/components/WorkoutList.vue';
+    import AddWorkout from '@/components/AddWorkout.vue';
+</script>
+<template>
+    <div>
+        <h1 class="title has-text-left">Friends Activity</h1>
+        <div class="columns is-centered">
+            <div class="column is-half">
+                <!-- Adding activity -->
+                <AddWorkout></AddWorkout>
+                <!-- Display activity list -->
+                <WorkoutList></WorkoutList>
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped>
+.column {
+        border: none;
+        box-shadow: none;
+    }
+</style>
