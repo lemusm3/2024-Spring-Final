11383fd9fe14305303fbb7a04a190810f227ae01
lemusm3 <lemusm3@newplatz.edu>
2024-05-13 13:01:09 -0400

    Update getUsers function to use correct User type and add searchUser function

--- client/src/model/users.ts
+++ client/src/model/users.ts
@@ -1,6 +1,8 @@
 import data from "../data/users.json"
+import { api } from "./myFetch";
 
-export interface UserRoot {
+export interface User {
+    id: number
     firstName: string
     lastName: string
     handle: string
@@ -8,7 +10,12 @@ export interface UserRoot {
     admin: boolean 
     image: string
   }
-export function getUsers(): UserRoot[] {
-    return data.items;
+export function getUsers(): User[] {
+    return data.items.map(item => ({ ...item, id: 0 }));
   }
+  export async function searchUser(term: string){
+    const data=await api<User[]>(`users/search?q=${term}`);
+    return data ? data.data : [];
+  }
+  
   
