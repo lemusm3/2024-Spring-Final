07ce4ea9f8f78826d6926999ee49dc60a43df3ec
lemusm3 <lemusm3@newplatz.edu>
2024-05-16 14:54:55 -0400

    Use Oruga in app

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,10 +1,15 @@
 import './assets/main.css'
+
 import { createApp } from 'vue'
+import Oruga from '@oruga-ui/oruga-next'
+import '@oruga-ui/theme-bulma/dist/bulma.css';
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-app.use(router)
+app
+    .use(router)
+    .use(Oruga)
 
 app.mount('#app')
