4477e7ca92430d57e95e76982f7f9af4947eecbb
lemusm3 <lemusm3@newplatz.edu>
2024-05-16 14:55:09 -0400

    Updated search for oruga

--- client/src/pages/Search.vue
+++ client/src/pages/Search.vue
@@ -1,60 +1,87 @@
-
 <script setup lang="ts">
 import { ref } from 'vue';
-import { type User } from '@/model/users';
+import { type User, getUsers } from '@/model/users';
 import { searchUser } from '@/model/users';
+import { computed } from 'vue';
 
+const users = ref([] as User[]);
 const info = ref([] as User[]);
-const searches=ref('');
-const nothing=ref('null');
 
-function findUser(string: string){
-  searchUser(string).then((u)=>info.value=u);
+//fetching the data from the model
+async function fetchData() {
+  users.value = await getUsers();
 }
 
+fetchData().then(() => (users.value));
+searchUser("null").then((u)=>info.value=u);
+
+//----------------------------------------------
+
+const data=users;
+const name = ref("");
+const selected = ref();
+
+const filteredDataArray = computed(() =>
+    data.value.filter(
+        (option) =>
+            option.toString().toLowerCase().indexOf(name.value.toLowerCase()) >=
+            0,
+    ),
+);
 
 </script>
 
 <template>
+  <section>
+    <o-field label="Find a User">
+        <o-autocomplete
+            v-model="name"
+            rounded
+            expanded
+            placeholder="Search for a User"
+            icon="Search"
+            clearable
+            open-on-focus
+            :data="filteredDataArray"
+            :field="'firstName'"  
+            @select="(option: User) => (selected = option)">
+            <template #empty>Try another User!</template>
+        </o-autocomplete>
+    </o-field>
 
-
-<!--Add a search bar-->
-<div class="field">
-  <div class="control">
-    <input class="input" type="text" placeholder="Search" v-model="searches">
-  </div>
-</div>
-  <p>You Searched for: {{ searches }}</p>
-
-  <button class="button is-primary" style="margin: 10px;" @click="findUser(searches)">Search</button>
-  <button class="button is-primary" style="margin: 10px;" @click="findUser(nothing); searches='';">Clear</button>
-
-
-    <div class="Search">
-          
-
-      <div class="card" v-for="item in info" :key="item.firstName">
-        <div class="media-content">
-          <p class="title is-4">{{ item.firstName }}</p>
+    <div v-if="selected">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">{{ selected.firstName }} {{ selected.lastName }}</h5>
         </div>
-        <div class="card-image">
-          <figure class="image" style="width: 200px;">
-        <img :src="item.image" alt="Product image" style="width: 100%;">
-          </figure>
-        </div>
-        <div class="card-content">
-          <div class="media">
-        
-          </div>
-        </div>
+        <img :src="selected.image" alt="User Image" class="card-img-top">
       </div>
-
-
-
     </div>
+
+  </section>
+    
   </template>
   
   <style>
+
+
+.card-img-top{
+  width:50px;
+}
+
+.field{
+  margin: 10px;
+}
+.o-autocomplete {
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  padding: 10px;
+  font-size: 16px;
+}
+
+.o-autocomplete .dropdown-item {
+  padding: 10px;
+}
 
   </style>
   
